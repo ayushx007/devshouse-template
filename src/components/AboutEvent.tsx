ea45ce87658d58309a4e85c3f790491a6ea50773
RITESHP36 <codingritesh36@gmail.com>
2023-10-22 20:34:41 +0530

    About event Component initial commit
    Add all the elements as per the figma design

---
+++ src/components/AboutEvent.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { MdSend } from "react-icons/md";
+import { BiSolidMicrophone } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const AboutEvent = () => {
+	return (
+		<div className="mx-16">
+			<div className="flex my-4 gap-2">
+				<GiHamburgerMenu color="black" size="25" />
+				<p className="font-semibold">Bard</p> 
+                <p className="rounded-md border-2 border-[#5392F6] text-xs py-0.5 px-2 text-[#5392F6]">Experiment</p>
+			</div>
+			<div className="bg-[#F3F6FC] px-8 py-2 rounded-lg">
+				<img src="\assets\gif.png" alt="bard Gif" className="h-6"/>
+				<p className="font-bold text-2xl ">Hello again! DevMania ‘24</p>
+				<p className="p-4 text-neutral-500 text-sm font-medium">
+					DevMania Hackathon by GDSC VIT Chennai, an annual tech innovation
+					event, calls upcoming engineers, designers, and entrepreneurs from
+					across the country to explore new ideas for the future. It offers
+					students the opportunity to create, connect, and showcase their
+					innovative solutions to solve real-world challenges.
+				</p>
+				<div className="grid grid-cols-3 gap-6 mb-2">
+					<div className="bg-white rounded-xl p-4">
+						<p className="text-xl font-bold mb-2 bg-gradient-to-r from-[#CF8D7C] to-[#A77CB4] text-transparent bg-clip-text">
+							Design
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							Ideathon
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							innovative Thinking
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4   rounded-full w-fit font-medium">
+							Conceptualization
+						</p>
+					</div>
+					<div className="bg-white rounded-xl p-4">
+						<p className="text-xl font-bold mb-2 bg-gradient-to-r from-[#A77CB4] to-[#7F7DD1] text-transparent bg-clip-text">
+							Develop
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							Collaboration
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							Tech Stack Selection
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4   rounded-full w-fit font-medium">
+							Iteration
+						</p>
+					</div>
+					<div className="bg-white rounded-xl p-4">
+						<p className="text-xl font-bold mb-2 bg-gradient-to-r from-[#7F7DD1] via-[#548DD9] to-[#548DD9] text-transparent bg-clip-text">
+							Deploy
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							Implementation
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4  mb-2 rounded-full w-fit font-medium">
+							Scalability
+						</p>
+						<p className="bg-[#F3F6FC] py-2 px-4   rounded-full w-fit font-medium">
+							Strategies
+						</p>
+					</div>
+				</div>
+				<div className="py-2 flex mb-2">
+					<div className="w-full bg-white text-sm py-2 px-4 rounded-full mx-6 text-neutral-500 border-2 border-black flex justify-between">
+						<p className="ml-4">Why is DevMania the go-to hackathon event?</p>
+						<BiSolidMicrophone size="20" />
+					</div>
+					<div className="flex items-center ">
+						<MdSend color="black" size="25" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AboutEvent;
