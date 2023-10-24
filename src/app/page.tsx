c86eaa4aeb2471f6d1c08433d3eca4496b2651c9
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-10-25 01:46:24 +0530

    previous event cubes and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
 import AboutEvent from '../components/AboutEvent';
+import PrevEvents from '@/components/PrevEvents';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <AboutEvent />
+      <PrevEvents/>
     </div>
   )
 }
