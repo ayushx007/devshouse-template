7a2e41c4c9b398c06ef1b28d482cfcc45c30c0cc
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2023-10-25 16:51:55 +0530

    about vit secrion (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Navbar from '../components/Navbar';
 import AboutEvent from '../components/AboutEvent';
 import PrevEvents from '@/components/PrevEvents';
 import AboutGDSC from '@/components/AboutGDSC';
-
+import AboutVit from '@/components/AboutVIT';
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <AboutEvent />
+      <AboutVit />
       <AboutGDSC/>
       <PrevEvents/>
     </div>
