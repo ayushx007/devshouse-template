b13da9fcaaf272c22f0c0e5f8f400ed99ec5babd
Vishesh0504 <visheshdpanchal@gmail.com>
2023-10-25 21:46:33 +0530

    Previous Events section made

--- src/components/PrevEvents.tsx
+++ src/components/PrevEvents.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { FC } from 'react'
-
+// import prevEvent1 from '../assets/images/prev-events1.png'
 interface PrevEventsProps {
 
 }
@@ -51,6 +51,7 @@ const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor, style, x=0, y=
                     width: `${size}px`,
                     height: `${size}px`,
                     backgroundImage: `url(${image})`,
+                    backgroundSize: 'cover',
                     transform: `translateZ(${size / 2}px)`,
                     ...cubeStyle
                 }}
@@ -87,26 +88,55 @@ const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor, style, x=0, y=
 }
 const PrevEvents: FC<PrevEventsProps> = ({ }) => {
     return (
-        <div style={{ transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg) translate3d(500px, 0px, 0px) skew(0deg, 0deg)' }}>
-            <div className='flex justify-center items-center w-80'>
-                <Cube size={100} image={'https://picsum.photos/200'} bgColor='#0D9C58' borderColor='#59CE95' />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+        <div className='bg-bg-black min-h-full p-24 max-md:p-8'>
+            <div className='heading flex justify-center align-middle pt-10'>
+                <h1 className='text-[6em] font-bold text-heading-grey opacity-10 max-md:text-5xl'>GLIMPSES</h1>
+                <h1 className='text-[3em] font-medium text-white absolute z-20 pt-10 max-md:text-2xl max-md:pt-3'>Previous Events</h1>
             </div>
-            <div className='flex justify-center items-center w-64' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(260px, 0px, 100px) skew(0deg, 0deg)' }}>
-                <Cube size={100} image={'https://picsum.photos/200'} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+            <div className='flex'>
+                <div className='flex-col text-[#FF7266]'>
+                    <h1 className='text-6xl max-md:text-3xl font-medium '>4000+</h1>
+                    <h1 className='text-4xl max-md:text-xl font-medium '>Hackers</h1>
+                </div>
+                <div className='flex-col text-[#59CE95] ml-auto'>
+                    <h1 className='text-6xl max-md:text-3xl font-medium '>30+</h1>
+                    <h1 className='text-4xl max-md:text-xl font-medium '>Workshops</h1>
+                </div>
             </div>
-            <div className='flex justify-center items-center w-80' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(100px, 0px, 100px) skew(0deg, 0deg)' }}>
-                <Cube size={100} image={'https://picsum.photos/200'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+            <div className='flex max-h-[45vh] mt-24 max-md:scale-[0.6] max-md:-mt-12 scale-150 max-2xl:scale-90 max-sm:scale-50'>
+                <div className='first-letter:' style={{ transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg) translate3d(45vw, 0px, 0px) skew(0deg, 0deg) ' }}>
+                    <div className='flex justify-center items-center w-80'>
+                        <Cube size={100} image={'../../assets/prev1.jpg'} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube size={100} image={'../../assets/prev2.png'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube size={100} image={'../../assets/prev3.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+                    </div>
+                    <div className='flex justify-center items-center w-64' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(260px, 0px, 100px) skew(0deg, 0deg)' }}>
+                        <Cube size={100} image={'../../assets/prev4.jpeg'} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+                    </div>
+                    <div className='flex justify-center items-center w-80' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(100px, 0px, 100px) skew(0deg, 0deg)' }}>
+                        <Cube size={100} image={'https://picsum.photos/200'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                    </div>
+                    <div className='flex justify-center items-center w-64'>
+                        <Cube size={100} image={'https://picsum.photos/200'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                    </div>
+                </div>
             </div>
-            <div className='flex justify-center items-center w-64'>
-                <Cube size={100} image={'https://picsum.photos/200'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
-                <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+            <div className='flex'>
+                <div className='flex-col text-[#81B0FF]'>
+                    <h1 className='text-6xl max-md:text-3xl font-medium '>100+</h1>
+                    <h1 className='text-4xl max-md:text-xl font-medium '>Projects</h1>
+                </div>
+                <div className='flex-col text-[#FFD764] ml-auto'>
+                    <h1 className='text-6xl max-md:text-3xl font-medium '>500+</h1>
+                    <h1 className='text-4xl max-md:text-xl font-medium '>Hack Hours</h1>
+                </div>
             </div>
         </div>
+
     );
 }
 export default PrevEvents
