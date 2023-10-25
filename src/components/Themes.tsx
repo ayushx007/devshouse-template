310fa00cef930a8d25485dc645beb1237b8ce06e
Christan Wilbert <cwchristan@gmail.com>
2023-10-25 09:23:14 +0530

    Themes animation fix

--- src/components/Themes.tsx
+++ src/components/Themes.tsx
@@ -5,34 +5,34 @@ import { useState } from "react";
 const Theme = () => {
   const [hoverItem, sethoverItem] = useState(0);
   const deptDesc = [
-    <div className="h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Embark on a journey where the{" "}
       <span className="text-white text-2xl font-extrabold font-['Inter']">
         boundaries
       </span>{" "}
       of possibility are pushed to new horizons.
     </div>,
-    <div className="h-[130px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Uncover hidden insights in data to shape a smarter tomorrow through
       <span className="text-yellow-500"> analysis</span> and{" "}
       <span className="text-yellow-500">modeling.</span>
     </div>,
-    <div className="h-[170px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Reinvent <span className="text-blue-500">trust</span> and{" "}
       <span className="text-blue-500"> transparency </span>with decentralized
       technology solutions for a more secure future.
     </div>,
-    <div className="h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Create immersive, user-centered experiences that blendand
       <span className="text-red-500"> form </span> and
       <span className="text-red-500"> function.</span>
     </div>,
-    <div className="h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Empower change by coding the{" "}
       <span className="text-green-500"> power </span> of transformation to{" "}
       <span className="text-green-500"> streamline </span> processes.
     </div>,
-    <div className="h-[140px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
+    <div className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
       Fuel<span className="text-purple-600"> creative </span> thinking and
       <span className="text-purple-600"> collaboration </span>to develop
       groundbreaking ideas to drive industry evolution.
@@ -49,16 +49,7 @@ const Theme = () => {
           TRACKS
         </div>
       </div>
-
-      {/* <div className="h-[95px] mx-auto text-center px-4 text-white text-2xl font-normal font-['Inter'] italic">
-        Embark on a journey where the{" "}
-        <span className="text-white text-2xl font-extrabold font-['Inter']">
-          boundaries
-        </span>{" "}
-        of possibility are pushed to new horizons.
-      </div> */}
       {deptDesc[hoverItem]}
-
       <div
         style={{ backgroundImage: `url('/images/blend(${hoverItem}).png')` }}
         className={`flex justify-evenly items-center flex-wrap bg-[url('/images/blend(${hoverItem}).png')] bg-no-repeat h-[490px] `}
