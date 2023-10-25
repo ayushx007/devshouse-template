f33c5e379f74e05468ee81d8578fa0411dd4688e
Shaik Junaid Rizwan <126189309+junaid-rizwan@users.noreply.github.com>
2023-10-25 11:53:36 +0530

    added AboutGDSC section in the components section and improved the responsiveness of the aboutGDSC section (#8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar';
 import AboutEvent from '../components/AboutEvent';
 import PrevEvents from '@/components/PrevEvents';
+import AboutGDSC from '@/components/AboutGDSC';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <AboutEvent />
+      <AboutGDSC/>
       <PrevEvents/>
     </div>
   )
