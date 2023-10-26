17983fa5c2b2c4368b4e60d414e177b0000cd6fd
Vishesh0504 <visheshdpanchal@gmail.com>
2023-10-26 20:27:50 +0530

    added the previous events poster, added the anchor tag for cube, changed the font size of Tracks and themes to make it coherent and responsive with rest of the design

--- src/components/PrevEvents.tsx
+++ src/components/PrevEvents.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { FC } from 'react'
-// import prevEvent1 from '../assets/images/prev-events1.png'
 interface PrevEventsProps {
 
 }
@@ -45,17 +44,21 @@ const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor, style, x=0, y=
                 e.currentTarget.style.transform = `scale3d(1, 1, 1) rotateX(55deg) rotateY(0deg) rotateZ(33deg) translate3d(${x}px, ${y}px, ${z}px) skew(-10deg, 10deg)`;
             }}
         >
+        <a href='https://gdscvitc.eu.org/' >
             <div
                 style={{
                     position: 'absolute',
+                    top:'-3.2em',
+                    right:'3em',
                     width: `${size}px`,
                     height: `${size}px`,
                     backgroundImage: `url(${image})`,
                     backgroundSize: 'cover',
-                    transform: `translateZ(${size / 2}px)`,
+                    transform: `translateZ(${size/2}px)`,
                     ...cubeStyle
                 }}
             />
+            </a>
             <div
                 style={{
                     position: 'absolute',
@@ -106,22 +109,22 @@ const PrevEvents: FC<PrevEventsProps> = ({ }) => {
             <div className='flex max-h-[45vh] mt-24 max-md:scale-[0.6] max-md:-mt-12 scale-150 max-2xl:scale-90 max-sm:scale-50'>
                 <div className='first-letter:' style={{ transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg) translate3d(45vw, 0px, 0px) skew(0deg, 0deg) ' }}>
                     <div className='flex justify-center items-center w-80'>
-                        <Cube size={100} image={'../../assets/prev1.jpg'} bgColor='#0D9C58' borderColor='#59CE95' />
-                        <Cube size={100} image={'../../assets/prev2.png'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube size={100} image={'../../assets/prev1.jpeg'} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube size={100} image={'../../assets/prev2.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
                         <Cube size={100} image={'../../assets/prev3.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
                     </div>
                     <div className='flex justify-center items-center w-64' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(260px, 0px, 100px) skew(0deg, 0deg)' }}>
                         <Cube size={100} image={'../../assets/prev4.jpeg'} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
-                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+                        <Cube size={100} image={'../../assets/prev5.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
                     </div>
                     <div className='flex justify-center items-center w-80' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(100px, 0px, 100px) skew(0deg, 0deg)' }}>
-                        <Cube size={100} image={'https://picsum.photos/200'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube size={100} image={'../../assets/prev10.jpeg'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube size={100} image={'../../assets/prev9.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube size={100} image={'../../assets/prev8.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
                     </div>
                     <div className='flex justify-center items-center w-64'>
-                        <Cube size={100} image={'https://picsum.photos/200'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
-                        <Cube size={100} image={'https://picsum.photos/200'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                        <Cube size={100} image={'../../assets/prev7.jpeg'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                        <Cube size={100} image={'../../assets/prev6.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
                     </div>
                 </div>
             </div>
