b85a2123ea1d23ddcbe5f65930c187c6176e9724
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-11-06 00:05:58 +0530

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import devManiaLogo from "../../public/images/DevsHouse.svg";
+import DevHouse from "../../public/images/DevHouse.png";
 import calendarIcon from "../../public/images/cal-icon.png";
 import locationIcon from "../../public/images/location.png";
 import HeroImage1 from "../../public/gifs/1.gif";
@@ -13,25 +13,40 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex mt-10 h-[90vh] flex-col items-center justify-center gap-4 bg-[url('/images/background.png')] bg-cover bg-no-repeat bg-center">
-      <Image src={DevHouse} alt="devManiaLogo" className="px-12 md:p-0"></Image>
-      <div className="flex flex-row gap-8">
+    <div className="flex mt-10 h-[90vh] flex-col items-center justify-center gap-4 bg-white bg-cover bg-no-repeat bg-center">
+      <div className="absolute -right-52 rotate-90 -top-36">
+        <Image src={HeroImage1} alt="devManiaLogo"></Image>
+      </div>
+      <div className="absolute left-2 top-0">
+        <Image src={HeroImage2} width={430} alt="devManiaLogo"></Image>
+      </div>
+      <div className="absolute -left-10 -bottom-10">
+        <Image src={HeroImage3} width={430} alt="devManiaLogo"></Image>
+      </div>
+      <div className="absolute -right-8 -bottom-10">
+        <Image src={HeroImage4} width={430} alt="devManiaLogo"></Image>
+      </div>
+      <div className="absolute">
+        <Image
+          src={HeroImage5}
+          width={500}
+          alt="devManiaLogo"
+          className="opacity-40"
+        ></Image>
+      </div>
+      <Image
+        src={DevHouse}
+        alt="devManiaLogo"
+        className="px-12 md:p-0 z-50"
+      ></Image>
+      <div className="flex flex-row gap-8 z-50">
         <span className="flex flex-row items-center gap-2 ">
           <Image src={calendarIcon} alt="gcalicon"></Image>
-          <p className="font-Inter-800 text-md sm:text-2xl">
-            March 15th-17th 2024
-          </p>
+          <p className="font-Inter-800 text-md sm:text-2xl">15th March 2024</p>
         </span>
-        <span className="flex flex-row items-center gap-4">
+        <span className="flex flex-row items-center gap-2">
           <Image src={locationIcon} alt="location icon"></Image>
-          <Link
-            href="https://maps.app.goo.gl/N25y8LdXWP7zgfSM6"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-Inter-800 text-md sm:text-2xl hover:scale-110 transition-transform ease-out"
-          >
-            VIT Chennai
-          </Link>
+          <p className="font-Inter-800 text-md sm:text-2xl">Chennai</p>
         </span>
       </div>
       <span className="bg-blue-500 rounded-md font-bold text-2xl px-4 py-3 text-white hover:scale-110 transition-transform ease-out z-50">
