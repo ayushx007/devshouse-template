e8a4e3e8da53d4f5f7d29b635fd9693e97f7d897
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-11-30 21:10:51 +0530

    modify description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DevsHouse",
-  description: "A community of developers",
+  description: "Code. Connect. Create.",
 };
 
 export default function RootLayout({
