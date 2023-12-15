dcdcdeb215de4fb0b37e7f93ff05da4c26ddce14
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-12-15 17:41:10 +0530

    fixed faq color palette and themes padding issue (#23)
    * fixed colors
    
    * fixed tracks responsive
    
    * swapped icon

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -8,26 +8,31 @@ const FAQ = () => {
     yellow: "bg-yellow-200",
     green: "bg-green-200",
   };
-  const items = FAQData.map((props) => (
+  const items = FAQData.map((props, i) => (
     <Accordion.Item className="border-[#393b41] border" key={props.key} value={props.question}>
-      <Accordion.Control className={`text-${props.color}-500 hover:bg-inherit`} >{props.question}</Accordion.Control>
-      <Accordion.Panel className={colorClasses[props.color as keyof typeof colorClasses]}>
+      <Accordion.Control className={`text-${props.color}-500 hover:bg-inherit`}>{props.question}</Accordion.Control>
+      <Accordion.Panel className={`bg-${props.color}-200`}>
         <p>{props.answer}</p>
       </Accordion.Panel>
     </Accordion.Item>
   ));
   return (
     <div id="faqs" className="flex justify-center items-center bg-bg-black pt-10">
+      <div className="hidden text-yellow-500"></div>
+      <div className="hidden text-red-500"></div>
+      <div className="hidden text-blue-500"></div>
+      <div className="hidden text-green-500"></div>
+
       <div className="bg-bg-black w-[80%] h-auto">
         <h1 className="text-5xl font-Inter-500 text-white text-center mb-10">FAQs</h1>
         <div className="border border-[#393b41]">
-        <Accordion transitionDuration={500} className="bg-[#18191b] font-Inter-300">
-          {items}
-        </Accordion>
+          <Accordion transitionDuration={500} className="bg-[#18191b] font-Inter-300">
+            {items}
+          </Accordion>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default FAQ;
--- src/components/Themes.tsx
+++ src/components/Themes.tsx
@@ -6,42 +6,63 @@ import { useState } from "react";
 const Theme = () => {
   const [hoverItem, sethoverItem] = useState(0);
   const deptDesc = [
-    <div key="1" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
+    <div
+      key="1"
+      className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
       Embark on a journey where the{" "}
-      <span className="text-white text-2xl max-md:text-base font-extrabold">
-        boundaries
-      </span>{" "}
-      of possibility are pushed to new horizons.
+      <span className="text-white text-2xl max-md:text-base font-extrabold">boundaries</span> of possibility are pushed
+      to new horizons.
     </div>,
-    <div key="11" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Forge the future of finance with innovative solutions. Optimize transactions, enhance security, and revolutionize user experience.
+    <div
+      key="11"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Forge the future of finance with innovative solutions. Optimize transactions, enhance security, and revolutionize
+      user experience.
     </div>,
-    <div key="2" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Embrace a greener tomorrow with technology. Build solutions for eco-friendly practices, optimize energy efficiency, and champion environmental conservation.
+    <div
+      key="2"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Embrace a greener tomorrow with technology. Build solutions for eco-friendly practices, optimize energy
+      efficiency, and champion environmental conservation.
     </div>,
-    <div key="3" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Supercharge developer productivity. Craft tools and applications that streamline workflows, enhance collaboration, and empower developers to achieve more.
+    <div
+      key="3"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Supercharge developer productivity. Craft tools and applications that streamline workflows, enhance collaboration,
+      and empower developers to achieve more.
     </div>,
-    <div key="4" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Push the boundaries of hardware. Explore cutting-edge innovations, from IoT devices to embedded systems, and shape the future of tangible technology.
+    <div
+      key="4"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Push the boundaries of hardware. Explore cutting-edge innovations, from IoT devices to embedded systems, and shape
+      the future of tangible technology.
     </div>,
-    <div key="5" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Champion inclusivity through tech. Address accessibility challenges, promote diverse perspectives, and build a more inclusive tech landscape for all.
+    <div
+      key="5"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Champion inclusivity through tech. Address accessibility challenges, promote diverse perspectives, and build a
+      more inclusive tech landscape for all.
     </div>,
-    <div key="6" className="h-[160px] md:h-[100px] mx-auto text-center px-4 text-white text-2xl font-normal italic">
-      Unleash the power of open innovation. Collaborate on open-source projects, embrace transparency, and foster a vibrant community of tech creators.
+    <div
+      key="6"
+      className="h-auto md:h-[100px] mx-auto text-center px-4 text-white text-lg md:text-2xl font-normal italic"
+    >
+      Unleash the power of open innovation. Collaborate on open-source projects, embrace transparency, and foster a
+      vibrant community of tech creators.
     </div>,
   ];
 
   return (
-    <div id='theme' className="bg-[#242425] w-full py-24">
+    <div id="theme" className="bg-[#242425] w-full py-24 flex flex-col">
       <div className="flex justify-center items-center">
-        <div className="absolute text-white text-[3em] max-md:text-2xl font-medium">
-          Themes
-        </div>
-        <div className="opacity-10 text-neutral-200 text-[6em] font-extrabold max-md:text-5xl">
-          TRACKS
-        </div>
+        <div className="absolute text-white text-[3em] max-md:text-2xl font-medium">Themes</div>
+        <div className="opacity-10 text-neutral-200 text-[6em] font-extrabold max-md:text-5xl">TRACKS</div>
       </div>
       {deptDesc[hoverItem]}
       <div className="bg-[url('/images/blend(1).png" />
@@ -68,7 +89,7 @@ const Theme = () => {
           <Image
             width={40}
             height={40}
-            alt='Finance Icon'
+            alt="Finance Icon"
             src="/images/datascience icon.png"
             className="absolute top-[40px] left-[-16px]"
           />
@@ -87,8 +108,8 @@ const Theme = () => {
           <Image
             width={40}
             height={40}
-            alt= 'Environmental Sustainability Icon'
-            src="/images/blockchain icon.png"
+            alt="Environmental Sustainability Icon"
+            src="/images/development icon.png"
             className="absolute top-[40px] left-[-16px]"
           />
         </div>
@@ -105,8 +126,8 @@ const Theme = () => {
           <p>Developer Productivity</p>
           <Image
             width={40}
-            height={40} 
-            alt= 'Developer Productivity Icon'
+            height={40}
+            alt="Developer Productivity Icon"
             src="/images/ux design icon.png"
             className="absolute top-[25px] left-[-18px]"
           />
@@ -125,8 +146,8 @@ const Theme = () => {
           <Image
             width={40}
             height={40}
-            alt= 'Development Icon'
-            src="/images/development icon.png"
+            alt="Development Icon"
+            src="/images/blockchain icon.png"
             className="absolute top-[40px] left-[-16px]"
           />
         </div>
@@ -144,7 +165,7 @@ const Theme = () => {
           <Image
             width={40}
             height={40}
-            alt= 'Diversity and Inclusion Icon'
+            alt="Diversity and Inclusion Icon"
             src="/images/diversity.png"
             className="absolute top-[35px] left-[-16px]"
           />
@@ -163,7 +184,7 @@ const Theme = () => {
           <Image
             width={40}
             height={40}
-            alt= 'Open Innovation Icon'
+            alt="Open Innovation Icon"
             src="/images/open innovation icon.png"
             className="absolute top-[35px] left-[-16px]"
           />
