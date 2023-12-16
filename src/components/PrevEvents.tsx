2fccba40c8a1d43ca4f2c5c448efda1157bf6f78
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-12-17 00:01:46 +0530

    modify event redirect link

--- src/components/PrevEvents.tsx
+++ src/components/PrevEvents.tsx
@@ -10,13 +10,14 @@ interface CubeProps {
     image: string
     bgColor: string
     borderColor: string
+    link: string
     x?: number
     y?: number
     z?: number
     style?: React.CSSProperties
 }
 
-const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor, style, x=0, y=0,z=0 }) => {
+const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor,link, style, x=0, y=0,z=0 }) => {
     const cubeStyle = {
         backgroundColor: bgColor,
         animation: 'rotate 1s infinite linear',
@@ -44,7 +45,7 @@ const Cube: FC<CubeProps> = ({ size, image, bgColor, borderColor, style, x=0, y=
                 e.currentTarget.style.transform = `scale3d(1, 1, 1) rotateX(55deg) rotateY(0deg) rotateZ(33deg) translate3d(${x}px, ${y}px, ${z}px) skew(-10deg, 10deg)`;
             }}
         >
-        <a href='https://gdscvitc.eu.org/' >
+        <a href={link} target='_blank' >
             <div
                 style={{
                     position: 'absolute',
@@ -109,22 +110,22 @@ const PrevEvents: FC<PrevEventsProps> = ({ }) => {
             <div className='flex max-h-[45vh] mt-24 max-md:scale-[0.6] max-md:-mt-12 scale-150 max-2xl:scale-90 max-sm:scale-50'>
                 <div className='first-letter:' style={{ transform: 'rotateX(0deg) rotateY(0deg) rotateZ(0deg) translate3d(45vw, 0px, 0px) skew(0deg, 0deg) ' }}>
                     <div className='flex justify-center items-center w-80'>
-                        <Cube size={100} image={'../../assets/prev1.jpeg'} bgColor='#0D9C58' borderColor='#59CE95' />
-                        <Cube size={100} image={'../../assets/prev2.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
-                        <Cube size={100} image={'../../assets/prev3.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube link="#" size={100} image={'../../assets/prev1.jpeg'} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube link="https://www.instagram.com/p/CqpRaGhLCUT/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev2.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
+                        <Cube link="https://www.instagram.com/p/CsnKM-qvBeH/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev3.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#0D9C58' borderColor='#59CE95' />
                     </div>
                     <div className='flex justify-center items-center w-64' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(260px, 0px, 100px) skew(0deg, 0deg)' }}>
-                        <Cube size={100} image={'../../assets/prev4.jpeg'} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
-                        <Cube size={100} image={'../../assets/prev5.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+                        <Cube link="https://www.instagram.com/p/CWC9068rtJW/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev4.jpeg'} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
+                        <Cube link="https://www.instagram.com/p/CVC1axBMvJw/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev5.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#FBBC08' borderColor='#FFD764' x={-310} y={-5} />
                     </div>
                     <div className='flex justify-center items-center w-80' style={{ transform: 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)  translate3d(100px, 0px, 100px) skew(0deg, 0deg)' }}>
-                        <Cube size={100} image={'../../assets/prev10.jpeg'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                        <Cube size={100} image={'../../assets/prev9.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
-                        <Cube size={100} image={'../../assets/prev8.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube link="https://www.instagram.com/p/CkP_Ao3r2SJ/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev10.jpeg'} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube link="https://www.instagram.com/reel/CxyAQzVPXqN/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev9.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
+                        <Cube link="https://www.instagram.com/p/C0oxd9JLB55/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev8.jpeg'} style={{ marginTop: '160px', marginLeft: '-65px' }} bgColor='#EA4335' borderColor='#FF7266' x={-1205} y={-2200} z={-1215} />
                     </div>
                     <div className='flex justify-center items-center w-64'>
-                        <Cube size={100} image={'../../assets/prev7.jpeg'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
-                        <Cube size={100} image={'../../assets/prev6.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                        <Cube link="https://www.instagram.com/p/CtPC-6ISn9f/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev7.jpeg'} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
+                        <Cube link="https://www.instagram.com/p/CtbGXmkr9da/?igshid=ODhhZWM5NmIwOQ==" size={100} image={'../../assets/prev6.jpeg'} style={{ marginTop: '80px', marginLeft: '-65px' }} bgColor='#4285F4' borderColor='#81B0FF' x={-800} y={-825} z={-130} />
                     </div>
                 </div>
             </div>
